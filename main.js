c374b3c2e71d5e300d34d50ab44b6ad8e5acbbb0
Pedro Papp <105350125+pedropapp@users.noreply.github.com>
2024-07-22 00:53:58 +0100

    mobile fiz?

--- main.js
+++ main.js
@@ -2,3 +2,10 @@ AOS.init({
     duration: 1000,
     once: true
   });
+
+  const hamburger = document.querySelector('.hamburger');
+  const navLinks = document.querySelector('.nav-links');
+
+  hamburger.addEventListener('click', () => {
+      navLinks.classList.toggle('active');
+  });
